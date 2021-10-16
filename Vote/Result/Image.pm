e0b222d7dcd72fb5493af34069af316ff4e7cf32
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2021-10-16 19:10:47 +0200

    Improve result image object

--- Vote/Result/Image.pm
+++ Vote/Result/Image.pm
@@ -8,6 +8,10 @@ use Mo::utils qw(check_isa check_required);
 
 our $VERSION = 0.01;
 
+has competition => (
+	is => 'ro',
+);
+
 has image => (
 	is => 'ro',
 );
@@ -17,12 +21,24 @@ has number_of_votes => (
 	is => 'ro',
 );
 
+has section => (
+	is => 'ro',
+);
+
 sub BUILD {
 	my $self = shift;
+
+	# Check competition.
+	check_required($self, 'competition');
+	check_isa($self, 'competition', 'Data::Commons::Vote::Competition');
 
 	# Check image.
 	check_required($self, 'image');
 	check_isa($self, 'image', 'Data::Commons::Vote::Image');
+
+	# Check section.
+	check_required($self, 'section');
+	check_isa($self, 'section', 'Data::Commons::Vote::Section');
 
 	return;
 }
