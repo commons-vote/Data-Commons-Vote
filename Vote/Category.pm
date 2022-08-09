ff9690fe00b916bbf8beafd9dde3c43f7249745d
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2022-08-09 11:37:49 +0200

    Add section id to category

--- Vote/Category.pm
+++ Vote/Category.pm
@@ -4,11 +4,15 @@ use strict;
 use warnings;
 
 use Mo qw(build is);
-use Mo::utils qw(check_length check_required);
+use Mo::utils qw(check_length check_number check_required);
 
 our $VERSION = 0.01;
 
 has category => (
+	is => 'ro',
+);
+
+has section_id => (
 	is => 'ro',
 );
 
@@ -18,6 +22,9 @@ sub BUILD {
 	# Check category.
 	check_required($self, 'category');
 	check_length($self, 'category', 255);
+
+	# Check section id.
+	check_number($self, 'section_id');
 
 	return;
 }
