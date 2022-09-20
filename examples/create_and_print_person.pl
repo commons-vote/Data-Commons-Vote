3bdd413469701f03705403dc1682057ea7f554fe
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2022-09-21 00:04:35 +0200

    Update examples

--- examples/create_and_print_person.pl
+++ examples/create_and_print_person.pl
@@ -9,6 +9,7 @@ use DateTime;
 use Unicode::UTF8 qw(decode_utf8 encode_utf8);
 
 my $obj = Data::Commons::Vote::Person->new(
+        'email' => 'skim@cpan.org',
         'first_upload_at' => DateTime->new(
                 'day' => 18,
                 'month' => 7,
@@ -31,6 +32,7 @@ my $obj = Data::Commons::Vote::Person->new(
 # Print out.
 print 'Id: '.$obj->id."\n";
 print 'Name: '.encode_utf8($obj->name)."\n";
+print 'Email: '.$obj->email."\n";
 print 'First upload to Wikimedia Commons: '.$obj->first_upload_at."\n";
 print 'Wikimedia username: '.$obj->wm_username."\n";
 print "Roles:\n";
@@ -44,6 +46,7 @@ foreach my $role (@{$obj->roles}) {
 # Output:
 # Id: 1
 # Name: Michal Josef Špaček
+# Email: skim@cpan.org
 # First upload to Wikimedia Commons: 2009-07-18T21:05:00
 # Wikimedia username: Skim
 # Roles:
